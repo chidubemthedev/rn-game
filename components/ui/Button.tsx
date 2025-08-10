cecd5eb6d52a6b758bd03014c7d4012dd52bda41
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-08-10 10:17:06 +0100

    added custom button

--- components/ui/Button.tsx
+++ components/ui/Button.tsx
@@ -1,18 +1,48 @@
 import React from "react";
-import { StyleSheet, Text, View } from "react-native";
+import { Pressable, StyleSheet, Text, View } from "react-native";
 
 type Props = {
   title: string;
 };
 
 const Button = ({ title }: Props) => {
+  const pressHandler = () => {
+    console.log("Pressed");
+  };
+
   return (
-    <View>
-      <Text>{title}</Text>
+    <View style={styles.buttonOuterContainer}>
+      <Pressable
+        onPress={pressHandler}
+        style={styles.buttonInnerContainer}
+        android_ripple={{ color: "#640233" }}
+      >
+        <Text style={styles.buttonText}>{title}</Text>
+      </Pressable>
     </View>
   );
 };
 
 export default Button;
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  buttonOuterContainer: {
+    borderRadius: 4,
+    margin: 4,
+    overflow: "hidden",
+  },
+  buttonInnerContainer: {
+    backgroundColor: "#72063c",
+    paddingHorizontal: 16,
+    paddingVertical: 8,
+    elevation: 2,
+    shadowColor: "black",
+    shadowOffset: { width: 0, height: 2 },
+    shadowRadius: 6,
+    shadowOpacity: 0.25,
+  },
+  buttonText: {
+    color: "white",
+    textAlign: "center",
+  },
+});
