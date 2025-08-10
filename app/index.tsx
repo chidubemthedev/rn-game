a8c1ecd9213a2b04618302f7547b94ca03213520
Agulue Valentine <61975856+chidubemthedev@users.noreply.github.com>
2025-08-10 11:44:55 +0100

    added image background

--- app/index.tsx
+++ app/index.tsx
@@ -1,11 +1,18 @@
 import { LinearGradient } from "expo-linear-gradient";
-import { StyleSheet } from "react-native";
+import { ImageBackground, StyleSheet } from "react-native";
 import StartScreen from "./start-game";
 
 export default function HomeScreen() {
   return (
     <LinearGradient colors={["#4e0329", "#ddb52f"]} style={styles.container}>
-      <StartScreen />
+      <ImageBackground
+        source={require("@/assets/images/dices.jpg")}
+        resizeMode="cover"
+        style={styles.container}
+        imageStyle={{ opacity: 0.15 }}
+      >
+        <StartScreen />
+      </ImageBackground>
     </LinearGradient>
   );
 }
